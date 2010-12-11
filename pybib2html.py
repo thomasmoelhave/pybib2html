0c9c2d14224a2e9e6dc88c28cbe87c1dba7def81
Thomas Moelhave <thomas@scalgo.com>
2010-12-10 20:41:23 -0500

    Generate better output.
    Support reading and writing from files specified on the command line.

--- pybib2html.py
+++ pybib2html.py
@@ -3,8 +3,17 @@
 import sys
 import re
 
+publication_counter=1
+output_file=sys.stdout
+author_data = dict()
+
 def sort_by_year(y, x):
 	return int(x[1].fields['year']) - int(y[1].fields['year'])
+
+def print_output(s):
+	global output_file
+	output_file.write(s)
+
 
 def replace_all(text, dic):
 	for i, j in dic.iteritems():
@@ -16,21 +25,23 @@ def replace_all(text, dic):
 
 def print_tag(t,extra_args=""):
 	if extra_args == "":
-		sys.stdout.write("<"+t+">")
+		print_output("<"+t+">")
 	else:
-		sys.stdout.write("<"+t+" " + extra_args+">")
+		print_output("<"+t+" " + extra_args+">")
 
 def put_header():
 	open_tag("head");
-	print "<link href=\"http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css\" rel=\"stylesheet\" type=\"text/css\"/>"
-	print "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js\"></script>"
-	print "<script src=\"http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js\"></script>"
+#	print_output("<link href=\"http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css\" rel=\"stylesheet\" type=\"text/css\"/>"
+#	print_output("<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js\"></script>"
+#	print_output("<script src=\"http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js\"></script>"
   
-	print "  <script>"
-	print "  $(document).ready(function() {"
-	print "    $(\"#accordion\").accordion();"
-	print "  });"
-	print "  </script>"
+	#print_output("  <script>"
+	#print_output("  $(document).ready(function() {"
+	#print_output("    $(\"#accordion\").accordion();"
+	#print_output("  });"
+	#print_output("  </script>"
+	print_output("<script src=\"http://code.jquery.com/jquery-1.4.4.js\"></script>\n")
+	print_output("<link href=\"style.css\" type=text/css rel=stylesheet>\n")
 	close_tag("head");
 
 
@@ -52,19 +63,38 @@ def html_h3(x):
 	html_intag("h3",x)
 
 def html_i(x):
-	html_intag("i",x)
+	html_intag("em",x)
 
 def html_br():
-	close_tag("br")
-	print ""
+	print_tag("br/")
 
 def html_a(target,text):
 	open_tag("a","href=\""+target+"\"")
 	printtex(text)
 	close_tag("a")
 
+def printtex_replace_command(t,command,newtext,argc,commandprefix="\\\\"):
+	#e.g. t="bla blac \frac{x}{y}", command=frac arg_map=\\1 / \\2
+	parseargs=""
+	
+	for i in range(0,argc):
+		parseargs+="{([^{]*)}"
+	
+	pattern=commandprefix+command+parseargs
+
+	return re.sub(pattern,newtext,t)
+
+
+
 def printtex_text(t):
-	t=re.sub("\\\emph{([a-zA-Z ]*)}","<i>\\1</i>",t)
+	### remove comments
+	t=t.replace("\\%","&#37;") #first get rid of actual %'s in the input
+	t=re.sub("%.*","",t) #the remaining %'s are comments
+
+	t=printtex_replace_command(t,"emph","<em>\\1</em>",1)
+	
+	t=t.replace("\\&","&amp;") #first get rid of actual %'s in the input
+	t=t.replace("\\o{}","&oslash;")
 	t=t.replace("\\o{}","&oslash;")
 	t=t.replace("\\ae{}","&aelig;")
 	t=t.replace("\\aa{}","&aring;")
@@ -72,94 +102,30 @@ def printtex_text(t):
 	t=t.replace("\\end{itemize}","</ul>")
 	t=t.replace("\\item","<li>")
 	t=t.replace("{","")
+	t=t.replace("~"," ")
 	t=t.replace("}","")
-	sys.stdout.write(t)
+	t=t.replace("\\-","-")
+	#print_output(t)
+	return t
 
-def printtex_math_cmd_handle_frac(args):
-#	sys.stderr.write(args[0])
-	sys.stderr.write("\t\t\t\\frac{"+args[0]+"}{"+args[1]+"}\n")
-	return	""+args[0]+"/"+args[1]+""
-
-def printtex_math_cmd_handle_mathibf(args):
-	sys.stderr.write("\t\t\t\\mathibf{"+args[0]+"}\n")
-	return "<strong>"+args[0]+"</strong>"
-
-def printtex_math_cmd_handle_mathrm(args):
-	sys.stderr.write("\t\t\t\\mathrm{"+args[0]+"}\n")
-	return "</i>"+args[0]+"<i>"
-
-def printtex_math_cmd_handle_sub(args):
-	sys.stderr.write("\t\t\t_{"+args[0]+"}\n")
-	return "<sub>"+args[0]+"</sub>"
-
-def printtex_math_handlecommand(c,rest):
-	commands = {
-		"log" : (0,"</i>log<i>"),
-		"ell" : (0,"l"),
-		"delta" : (0,"&delta;"),
-		"sum" : (0,"&sum;"),
-		"Theta" : (0,"&Theta;"),
-		"Omega" : (0,"&Omega;"),
-		"dots" : (0,"..."),
-		"mathitbf" : (1,printtex_math_cmd_handle_mathibf),
-		"mathbf" : (1,printtex_math_cmd_handle_mathibf),
-		"mathrm" : (1,printtex_math_cmd_handle_mathrm),
-		"mathop" : (0,""),
-		"frac" : (2,printtex_math_cmd_handle_frac),
-		"" : (1,printtex_math_cmd_handle_sub), #corresponds to _
-	}
-
-	sys.stderr.write("\t\tHANDLING: \"" + c +"\" rest: " + rest + "\n")
-
-	if c in commands:
-		(argc,handler)=commands[c]
-		if argc==0:
-			return (handler,len(c))
-		args=[]
-		
-		#read argc arguments, MUST be enclosed in { }
-		args=re.findall(r'{([^{]*)}',rest)[0:argc]
-		skip=0
-		for a in args:
-			skip+=len(a)+2
-		return (handler(args),skip+len(c))
-	else:
-		sys.stderr.write("\t\tunknown function: "+c+"\n")
-		return ("",len(c))
-
-		
-	
-
-
-def printtex_math_readcommand(t,i):
-	for j in range(i,len(t)):
-		if not (t[j].isalpha()):
-			return (t[i:j],j)
-	return (t[i:len(t)],len(t))
-	
 
 def printtex_math(t):
 	t=t.replace("<","&lt;")
 	t=t.replace(">","&gt;")
-	open_tag("i")
-#	printtex_math_work(t)
-	sys.stderr.write("Handling: \"" +t+"\"\n")
-	i=0
-	while i < len(t):
-		c=t[i]
-		sys.stderr.write("\tNext char: t["+str(i)+"]=\""+c+"\"\n")
-		if c == "\\" or c == "_":
-			(command,unused)=printtex_math_readcommand(t,i+1)
-			(result,skip)=	printtex_math_handlecommand(command,t[i:len(t)])
-			sys.stderr.write("\thandled \""+t+"\" with command \""+command+"\" and skip " + str(skip)+"\n")
-			i+=skip+1 
-			sys.stdout.write(result)
-			continue
-		i+=1
-		sys.stdout.write(c)
-			
-	close_tag("i")
-
+	t=t.replace("\delta","&delta;")
+	t=t.replace("\dots","...")
+	t=t.replace("\Theta","&Theta;")
+	t=t.replace("\Omega","&Omega;")
+	t=t.replace("\log","</em>log<em>")
+	t=t.replace("\ell","l")
+	t=printtex_replace_command(t,"mathitbf","<strong>\\1</strong>",1)
+	t=printtex_replace_command(t,"mathbf","<strong>\\1</strong>",1)
+	t=printtex_replace_command(t,"mathrm","</em>\\1<em>",1)
+	t=printtex_replace_command(t,"frac","\\1/\\2",2)
+	t=printtex_replace_command(t,"_","<sub>\\1</sub>",1,"")
+	open_tag("em")
+	print_output(t)
+	close_tag("em")
 
 def printtex(t):
 	tokens=t.split('$')
@@ -168,134 +134,260 @@ def printtex(t):
 			if math:
 				printtex_math(tok)
 			else:
-				printtex_text(tok)
+				print_output(printtex_text(tok) )
 			math=not math
 
 def put_title(value):
-	open_tag("h3")
-	open_tag("a","href=\"#\"")
+	open_tag("strong")
+#	open_tag("a","href=\"#\"")
 	printtex(value.fields['title'])
-	close_tag("a")
-	close_tag("h3")
+#	close_tag("a")
+	close_tag("strong")
 
 
 def put_title_author(value):
 	put_title(value)
 	html_br()
-	printtex(value.fields['author'])
+	print_authors(value)
 
-def new_entry_begin():
-	print ""
+def new_entry_end(key,value):
+	if	"abstract" in value.fields:
+		key=key.replace(":","__")
+		print_output("<small><a href=\"#"+key+"\" id=\"hide_"+key+"\">Hide Details</a> / \n")
+		print_output("<a href=\"#"+key+"\" id=\"show_"+key+"\">Show Details</a></small>\n")
+		print_output("<script>\n")
+		print_output("$(\"#hide_"+key+"\").click(function () {\n")
+		print_output("  $(\"."+key+"\").hide(\"fast\", function () {\n")
+		print_output("    // use callee so don't have to name the function\n")
+		print_output("    $(this).prev().hide(\"normal\", arguments.callee); \n")
+		print_output("  });\n")
+		print_output("});\n")
+		print_output("$(\"#show_"+key+"\").click(function () {\n")
+		print_output("  $(\"."+key+"\").show(200);\n")
+		print_output(" });\n")
+		print_output("</script>\n")
 
-def new_entry_end():
-	print ""
+def new_entry_begin(key,value):
+	print_output("")
 
 
-def put_data_line(value,f):
+def put_data_line(value,f,f2=""):
 	if f in value.fields:
 		html_br()
 		html_i(value.fields[f])
-		sys.stdout.write(",")
-	sys.stdout.write(" "+value.fields['year'])
+		print_output(",")
+		if f2 in value.fields:
+			html_i(" "+value.fields[f2])
+			print_output(",")
+	print_output(" "+value.fields['year']+".")
 
+def print_authors(value):
+	global author_data
 
+	firstPerson=True
+	for p in value.persons.items()[0][1]:
+		if not firstPerson:
+			print_output(", ")
+		else:
+			firstPerson=False
+		name=""
+		first=True
+		for f in p.first():
+			name+=f+" "
+		for f in p.middle():
+			name+=f+" "
+		for f in p.last():
+			name+=f+" "
 
-def handle_default(value):
-	new_entry_begin()
+		link=None
+		for pattern in author_data:
+			link_=author_data[pattern]
+			if re.search(pattern,name) != None:
+				link=link_
+				
+		if link==None:
+			print_output(printtex_text(name[0:len(name)-1]))
+			sys.stderr.write("No link for: " + name + "\n")
+		else:
+			print_output('<a href=\"'+link+'\">')
+			print_output(printtex_text(name[0:len(name)-1]))
+			print_output('</a>')
 
-	put_title(value)
-	open_tag("div")
-	printtex(value.fields['author'])
+	print_output(".")
 
+def print_doi(value):
+	if 'doi' in value.fields:
+		html_intag("strong","DOI: ")
+		html_br()
+		doi=value.fields['doi']
+		print_output(" ")
+		html_a("http://dx.doi.org/"+doi, "["+doi+"]")
+
+def read_author_data(filename):
+	f=open(filename)
+	global author_data
+	for l in f:
+		test=l.split('=') # format is "author pattern"="email"
+		if len(test)!=2:
+			if (l != ""):
+				sys.stderr.write("Skipping author data line: " + l + "\n")
+			continue
+		name=test[0]
+		url=test[1]
+			
+		url=url.rstrip('\n')
+		author_data[name]=url
+	
+def handle_default(key,value):
+	new_entry_begin(key,value)
+
+	put_title_author(value)
 	if 'journal' in value.fields:
 		html_br()
 		html_i(value.fields['journal'])
-		print ","
 	if 'howpublished' in value.fields:
 		html_br()
 		html_i(value.fields['howpublished'])
-		print ","
-	print value.fields['year']
-	close_tag("div")
-	new_entry_end()
+	print_output(",")
+	print_output(value.fields['year']+".")
 
-def handle_article(value):
-	new_entry_begin()
-	put_title(value)
 	open_tag("div")
-	printtex(value.fields['author'])
-	put_data_line(value,"journal")
-
-	if 'doi' in value.fields:
-		html_a("http://dx.doi.org/"+value.fields['doi']," <strong>[DOI]</strong>")
-
-	close_tag("div")
-	new_entry_end()
-
-
-def handle_inproceedings(value):
-	new_entry_begin()
-	put_title(value)
-	open_tag("div")
-	printtex(value.fields['author'])
-	put_data_line(value,"booktitle")
-	if 'doi' in value.fields:
-		html_a("http://dx.doi.org/"+value.fields['doi']," <strong>[DOI]</strong>")
+	open_tag("div","class=\""+key.replace(":","__")+"\" style=\"display:none;\"")
 	if 'abstract' in value.fields:
-		html_br()
-		printtex(value.fields['abstract'])
-
+		print_abstract(value)
 	close_tag("div")
-	new_entry_end()
+	close_tag("div")
 
-def handle_techreport(value):
-	new_entry_begin()
-	put_title(value)
+	
+	new_entry_end(key,value)
+
+def handle_article(key,value):
+	new_entry_begin(key,value)
+	put_title_author(value)
+	put_data_line(value,"journal")
+	print_doi(value)
+	new_entry_end(key,value)
+
+def print_abstract(value):
+	html_intag("strong","Abstract:")
+	html_br()
+	printtex(value.fields['abstract'])
+	html_br()
+
+
+def handle_phdthesis(key,value):
+	new_entry_begin(key,value)
+	put_title_author(value)
+	put_data_line(value,"school")
 	open_tag("div")
-	printtex(value.fields['author'])
-	put_data_line(value,"institution")
-	if 'doi' in value.fields:
-		html_a("http://dx.doi.org/"+value.fields['doi']," <strong>[DOI]</strong>")
+	open_tag("div","class=\""+key.replace(":","__")+"\" style=\"display:none;\"")
+	if 'abstract' in value.fields:
+		print_abstract(value)
 	close_tag("div")
-	new_entry_end()
+	close_tag("div")
+	new_entry_end(key,value)
 
+def handle_inproceedings(key,value):
+	new_entry_begin(key,value)
+	put_title_author(value)
+	put_data_line(value,"booktitle")
+	open_tag("div")
+	open_tag("div","class=\""+key.replace(":","__")+"\" style=\"display:none;\"")
+	if 'abstract' in value.fields:
+		print_abstract(value)
+	print_doi(value)
+	close_tag("div")
+	close_tag("div")
+	new_entry_end(key,value)
 
+def handle_techreport(key,value):
+	new_entry_begin(key,value)
+	put_title_author(value)
+	put_data_line(value,"institution","number")
+	open_tag("div")
+	open_tag("div","class=\""+key.replace(":","__")+"\" style=\"display:none;\"")
+	if 'abstract' in value.fields:
+		print_abstract(value)
+	print_doi(value)
+	close_tag("div")
+	close_tag("div")
+	new_entry_end(key,value)
+
+def handle_values(l):
+	global publication_counter
+	l_sorted = sorted(l, cmp=sort_by_year)
+	handlers = {'article':handle_article,'inproceedings':handle_inproceedings,'techreport':handle_techreport,'phdthesis':handle_phdthesis}
+	for key, value in l_sorted:
+		bibtex_class = value.type
+		open_tag("li")
+		if (bibtex_class in handlers):
+			handlers[bibtex_class](key,value)
+		else:
+			sys.stderr.write("No handler for: " + bibtex_class+", using default\n")
+			handle_default(key,value)
+		close_tag("li")
+		html_br()
+		publication_counter+=1
+
+def handle_types(list_of_types,typemaps, description):
+	print_output("<h2>"+description+"</h2>")
+#	open_tag("div","id=\"accordion\"")
+	open_tag("ol","start=\""+str(publication_counter)+"\"")
+	for l in list_of_types:
+		handle_values(typemaps[l])
+	close_tag("ol")
+
+#	close_tag("div")
 
 
 	
 def main():
+	import getopt
+	options,remainder=getopt.getopt(sys.argv[1:], 'i:o:a:',['input=','output=',"authorfile="])
+
+
 	from pybtex.database.input import bibtex
 	from operator import itemgetter, attrgetter
 	import pprint
 
 	parser = bibtex.Parser()
-	bib_data = parser.parse_file('mypapers.bib')
-	bib_sorted = sorted(bib_data.entries.items(), cmp=sort_by_year)
+	#bib_data = parser.parse_file('mypapers.bib')
+	global output_file
+	for opt,arg in options:
+		if opt in ('-o', '--output'):
+			output_file = open(arg,'w')
+		if opt in ('-i', '--input'):
+			input_filename = arg
+		if opt in ('-a', '--authors'):
+			author_filename = arg
+			read_author_data(author_filename)
 
-#	print "<ul>"
+	bib_data = parser.parse_file(input_filename)
+
 	open_tag("html")
 	put_header()
 	open_tag("body")
-	handlers = {'article':handle_article,'inproceedings':handle_inproceedings,'techreport':handle_techreport}
-	
-	open_tag("div","id=\"accordion\"")
-	for key, value in bib_sorted:
-		bibtex_class = value.type
-		if (bibtex_class in handlers):
-			handlers[bibtex_class](value)
-		else:
-			sys.stderr.write("No handler for: " + bibtex_class+", using default\n")
-			handle_default(value)
 
-	close_tag("div")
-	#print "</ul>"
+	from collections import defaultdict
+	typemaps=defaultdict(list)
+
+	for key, value in bib_data.entries.items():
+		bibtex_class = value.type
+		typemaps[bibtex_class].append((key,value))
+
+	
+	handle_types(["phdthesis"],typemaps,"Dissertation")
+	handle_types(["inproceedings"],typemaps,"Conference Papers")
+	handle_types(["article","techreport"],typemaps,"Other Papers")
+	handle_types(["misc"],typemaps,"Abstracts")
 
 
 	html_br()
-	print "<small>Generated by "
+	print_output("<small>Generated by ")
 	html_a("https://github.com/thomasmoelhave/pybib2html","pybib2html")
 
 	close_tag("body")
 	close_tag("html")
 
 main()
+
